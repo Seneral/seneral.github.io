89c6d68004e6c660bd29706f1930c0d15151c88d
Seneral <12800840+Seneral@users.noreply.github.com>
2017-02-01 23:33:06 +0100

    - Added side navigation list for jumping between paragraphs - Navigation list follows top window edge - Only used in TC2 Node Painter Documentation so far

--- BUILD/js/page.js
+++ BUILD/js/page.js
@@ -1,24 +1,68 @@
-function updateHeader () 
+function getNumericalProp (element, property, defaultValue) 
 {
-	/* Transparent header over image (have to remove headerHeight offset)
-	$("#debug").text("scrollTop:" + $(document).scrollTop());
-	if ($(document).scrollTop() == 0) {
-		$('.header').css( { 
-			'background-color':'transparent',
-			'color':'black'
-		});
-	} else {
-		$('.header').css({ 
-			'background-color':'var(--backCol)',
-			'color':'var(--frontCol)'
-		});
-	}*/
+	try {
+		return parseFloat(window.getComputedStyle(element, null).getPropertyValue(property));
+	} catch (err) {
+		return defaultValue;
+	}
+}
+function getDefaultFontSize () 
+{
+	return getNumericalProp (document.body, 'font-size', 16);
+}
+function resetProperty (element, property) {
+	// Both should work
+	element[0].style.removeProperty (property);
+	element.css(property, '');
 }
 
-function main () 
-{
-	$("*").removeClass ("hideOnStart");
 
+function updateScrollPos () 
+{
+	var $window = $(window), $header = $('.header'), $sideNav = $('.sideNav');
+	var defaultOffset = $sideNav.offset ().top, defaultMargin = parseFloat ($sideNav.css('margin-top')), padding = 85, defaultFontSize = getDefaultFontSize ();
+	//$(".debug").text ("Values: defaultOffset: " + defaultOffset + "; defaultMargin: " + defaultMargin + "!");
+	var update = function () {
+		/* Side Navigation box */
+		//$(".debug").text ($window.width () + " > " + (90*defaultFontSize) +" && " + $window.scrollTop() + " > " + (offset - padding + defaultMargin));
+		if ($window.width () > (90*defaultFontSize) && $window.scrollTop() > (defaultOffset + defaultMargin - padding)) {
+			$sideNav.css('marginTop', $window.scrollTop() - (defaultOffset - padding)); // Overwrite css margin-top to push sideNav down
+			//$(".debug").text ("Adjusting with current offset " + $sideNav.offset ().top + " and current margin " + parseFloat ($sideNav.css('margin-top')) + 
+			//	" and with defaultOffset: " + defaultOffset + " and defaultMargin " + defaultMargin + "!");
+		} else {
+			resetProperty ($sideNav, 'marginTop');
+			resetProperty ($sideNav, 'margin-top');
+			//$(".debug").text ("Reset to defaultOffset: " + defaultOffset + " and defaultMargin " + defaultMargin + "!");
+		}
+		/* Make fixed header transparent over image (have to remove headerHeight offset) */
+		/*$("#debug").text("scrollTop:" + $(document).scrollTop());*/
+		/*if ($window.scrollTop() == 0) {
+			$header.css( { 
+				'background-color':'transparent',
+				'color':'black'
+			});
+		} else {
+			$header.css({ 
+				'background-color':'var(--backCol)',
+				'color':'var(--frontCol)'
+			});
+		}*/
+	};
+	$(window).scroll(update);
+	$(window).resize(function () {
+		/* Update default values  */
+		resetProperty ($sideNav, 'marginTop');
+		resetProperty ($sideNav, 'margin-top');
+		defaultMargin = parseFloat ($sideNav.css('margin-top'));
+		defaultOffset = $sideNav.offset ().top;
+		update ();
+		//$(".debug").text ("Resized! Updating values to defaultOffset: " + defaultOffset + " and defaultMargin: " + defaultMargin + "!");
+	});
+	update ();
+}
+
+function setupHiSrc () 
+{
 	$("img.hisrc, .hisrc img").hisrc({
 		minKbpsForHighBandwidth: 300,
 		speedTestUri: "img/50k.jpg",
@@ -38,9 +82,15 @@ function main ()
 		// Debug
 		/*forcedBandwidth: true, */
 	});
-	
-	updateHeader ();
-	$(window).scroll(updateHeader);
+}
+
+function main () 
+{
+	$("*").removeClass ("hideOnStart");
+
+	setupHiSrc ();
+
+	updateScrollPos ();
 };
 
 $.hisrc.speedTest();
